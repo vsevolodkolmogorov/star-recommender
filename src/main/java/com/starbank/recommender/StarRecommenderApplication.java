75be669a43e8dbba4776df26a10d33c73cf917c6
vilar <loadslowly@gmail.com>
2024-12-19 18:49:25 +0500

    Initial commit: Add basic project structure

---
+++ src/main/java/com/starbank/recommender/StarRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.starbank.recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starbank/recommender/StarRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbank.recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
